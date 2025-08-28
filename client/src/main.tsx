2768b2333d709cf89058811dab14cd76af069813
Benedict Maigida <teyeiesla@gmail.com>
2025-08-28 11:59:48 +0100

    feat: fixed game ui

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { sdk } from "@farcaster/miniapp-sdk";
 
 createRoot(document.getElementById("root")!).render(<App />);
 
-(async () => {
-    try {
-      await sdk.actions.ready();
-      console.log('Farcaster overlay dismissed.');
-    } catch {
-      console.warn('Farcaster SDK not available (likely dev mode).');
-    }
-  })();
-
-//   if (import.meta.env.DEV) {
-//       import("./debugOverlay");
-//   }
